af70fc3d6023eabaca5a937e1d8f338471738c12
Aron Kuch <AronKuch@gmail.com>
2021-11-26 08:00:51 -0500

    Started refactoring, but next video does it better.

--- src/server.js
+++ src/server.js
@@ -4,32 +4,51 @@ import { MongoClient } from "mongodb";
 
 const app = express();
 
+// returns {client, db, articleInfo}
+// client so that you can later client.close()
+// db to make updates/inserts, etc.
+// articleInfo is a JSON of the article
+// This might go away because I don't know how much it helps.
+const getArticle = async (articleName) => {
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+  const db = client.db("back-blog");
+  const articleInfo = await db
+    .collection("articles")
+    .findOne({ name: articleName });
+  return { client, db, articleInfo };
+};
+
 app.use(bodyParser.json());
 
 app.get("/api/articles/:name", async (req, res) => {
   try {
     let articleName = req.params.name;
-    const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
-    let db = client.db("back-blog");
-    let articlesInfo = await db
-      .collection("articles")
-      .findOne({ name: articleName });
-    res.status(200).json(articlesInfo);
+    let { client, db, articleInfo } = await getArticle(articleName);
+    res.status(200).json(articleInfo);
     client.close();
   } catch (error) {
     res.status(500).json({ message: "Error connecting to db ", error });
   }
 });
 
-app.post("/api/articles/:name/upvote", (req, res) => {
+app.post("/api/articles/:name/upvote", async (req, res) => {
   let articleName = req.params.name;
-
-  articlesInfo[articleName].upvotes += 1;
+  let { client, db, articleInfo } = await getArticle(articleName);
+  await db
+    .collection("articles")
+    .updateOne(
+      { _id: articleInfo._id },
+      { $set: { upvotes: ++articleInfo.upvotes } }
+    );
+  const updatedArticleInfo = await db
+    .collection("articles")
+    .findOne({ name: articleName });
   res
     .status(200)
     .send(
-      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`
+      `${updatedArticleInfo.name} now has ${updatedArticleInfo.upvotes} upvotes!`
     );
+  ("");
 });
 
 app.post("/api/articles/:name/comments", (req, res) => {
